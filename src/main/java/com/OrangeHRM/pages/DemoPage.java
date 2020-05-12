c63288b8c082f0ceb301c7f4271dd1703a112135
Tushar <manik.k6388@gmail.com>
2020-05-12 20:49:30 +0530

    Cnstructor added to demopage-DealsBranch

--- src/main/java/com/OrangeHRM/pages/DemoPage.java
+++ src/main/java/com/OrangeHRM/pages/DemoPage.java
@@ -1,6 +1,11 @@
 package com.OrangeHRM.pages;
 
 public class DemoPage {
+	
+	public DemoPage() {
+		System.out.println("Cnstructor added to demopage-DealsBranch");
+	}
+	
 	public void demo() {
 		System.out.println("New Page added");
 	} 
